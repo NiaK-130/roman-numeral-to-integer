e27c517349dd8e98f900af1b4eb470e912bff164
Nia K <nsjkappen@gmail.com>
2022-01-28 17:04:12 -0800

    commit

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,6 +1,43 @@
 function romanNumeral(string) {
-  // type your code here
+ 
+  const romans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < string.length; i++){
+
+    const curr = romans[string[i]];
+    const next = romans[string[i+1]]
+
+    if (curr < next){
+      result = result + next - curr;
+      i++
+    } else{
+      result = result + curr
+    }
+
+  }
+  return result; 
+      
 }
+
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -14,8 +51,8 @@ if (require.main === module) {
 
   console.log("");
 
-  console.log("Expecting: 402");
-  console.log(romanNumeral('CDII'));
+  console.log("Expecting: 409");
+  console.log(romanNumeral('CDIX'));
 }
 
 module.exports = romanNumeral;
